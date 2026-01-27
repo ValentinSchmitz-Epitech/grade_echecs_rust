0d758e403598dbeccbf46996c616f0842f6f54e2
Stanis Serpentini <stanis.serpentini@epitech.eu>
2026-01-27 11:14:55 +0100

    ADDED: The init_board function and gitignore

--- src/main.rs
+++ src/main.rs
@@ -4,3 +4,24 @@
 // File description:
 // main
 //
+
+#[derive(Copy,Clone, Debug)]
+struct Piece {
+    cords: [usize; 2],
+}
+
+fn init_board() -> Vec<[Piece; 8]>{
+    let mut board = vec![[Piece{cords: [0, 0]}; 8]; 8];
+
+    for i in 0..8 {
+        for j in 0..8 {
+            (board[i][j]).cords = [i, j];
+        }
+    }
+    return board;
+}
+
+fn main() {
+    let mut board = init_board();
+    println!("{:?}", board);
+}
