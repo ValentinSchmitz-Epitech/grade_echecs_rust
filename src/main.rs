fd6baf754268d5ef6b12d54bc2ba7337991d605c
Stanis Serpentini <stanis.serpentini@epitech.eu>
2026-02-02 11:56:02 +0100

    FIXED: The struture and init_board function

--- src/main.rs
+++ src/main.rs
@@ -5,76 +5,66 @@
 // main
 //
 
-#[derive(Copy,Clone, Debug)]
+#[derive(Copy,Clone, Debug, PartialEq, Eq)]
 enum Type {
-    Pawn,
-    Rook,
-    Knight,
-    Bishop,
-    Queen,
-    King,
+    Wpawn,
+    Wrook,
+    Wknight,
+    Wbishop,
+    Wqueen,
+    Wking,
+    Bpawn,
+    Brook,
+    Bknight,
+    Bbishop,
+    Bqueen,
+    Bking,
+    None,
 }
 
 #[derive(Copy,Clone, Debug)]
 struct Piece {
     cords: [usize; 2],
     type_p: Type,
-    dead: bool,
 }
 
-fn init_board() -> ([Piece; 16], [Piece; 16]){
-    let mut w_pieces: [Piece; 16] = [Piece{
-        cords: [0, 0], type_p: Type::Pawn, dead: false}; 16];
-    let mut b_pieces: [Piece; 16] = [Piece{
-        cords: [0, 0], type_p: Type::Pawn, dead: false}; 16];
-
-    w_pieces[8].type_p = Type::Rook;
-    w_pieces[9].type_p = Type::Rook;
-    w_pieces[10].type_p = Type::Knight;
-    w_pieces[11].type_p = Type::Knight;
-    w_pieces[12].type_p = Type::Bishop;
-    w_pieces[13].type_p = Type::Bishop;
-    w_pieces[14].type_p = Type::Queen;
-    w_pieces[15].type_p = Type::King;
-
-    b_pieces[8].type_p = Type::Rook;
-    b_pieces[9].type_p = Type::Rook;
-    b_pieces[10].type_p = Type::Knight;
-    b_pieces[11].type_p = Type::Knight;
-    b_pieces[12].type_p = Type::Bishop;
-    b_pieces[13].type_p = Type::Bishop;
-    b_pieces[14].type_p = Type::Queen;
-    b_pieces[15].type_p = Type::King;
+fn init_board() -> [[Piece; 8]; 8] {
+    let mut board: [[Piece; 8]; 8] = [[Piece{
+        cords: [0, 0], type_p: Type::None}; 8]; 8];
 
     for i in 0..8 {
-        w_pieces[i].cords = [1, i];
-        b_pieces[i].cords = [6, i];
+        for j in 0..8 {
+            board[i][j].cords = [i, j];
+        }
     }
-    w_pieces[8].cords = [0, 0];
-    w_pieces[9].cords = [0, 7];
-    w_pieces[10].cords = [0, 1];
-    w_pieces[11].cords = [0, 6];
-    w_pieces[12].cords = [0, 2];
-    w_pieces[13].cords = [0, 5];
-    w_pieces[14].cords = [0, 4];
-    w_pieces[15].cords = [0, 3];
+    board[0][0].type_p = Type::Wrook;
+    board[0][7].type_p = Type::Wrook;
+    board[0][1].type_p = Type::Wknight;
+    board[0][6].type_p = Type::Wknight;
+    board[0][2].type_p = Type::Wbishop;
+    board[0][5].type_p = Type::Wbishop;
+    board[0][4].type_p = Type::Wqueen;
+    board[0][3].type_p = Type::Wking;
 
-    b_pieces[8].cords = [7, 0];
-    b_pieces[9].cords = [7, 7];
-    b_pieces[11].cords = [7, 6];
-    b_pieces[12].cords = [7, 2];
-    b_pieces[10].cords = [7, 1];
-    b_pieces[13].cords = [7, 5];
-    b_pieces[14].cords = [7, 4];
-    b_pieces[15].cords = [7, 3];
+    board[7][0].type_p = Type::Brook;
+    board[7][7].type_p = Type::Brook;
+    board[7][6].type_p = Type::Bknight;
+    board[7][2].type_p = Type::Bknight;
+    board[7][1].type_p = Type::Bbishop;
+    board[7][5].type_p = Type::Bbishop;
+    board[7][4].type_p = Type::Bqueen;
+    board[7][3].type_p = Type::Bking;
 
-    return (w_pieces, b_pieces);
+    for i in 0..8 {
+        board[1][i].type_p = Type::Wpawn;
+        board[6][i].type_p = Type::Bpawn;
+    }
+
+    return board;
 }
 
 fn main() {
-    let tmp: ([Piece; 16], [Piece; 16]) = init_board();
-    let mut w_pieces: [Piece; 16] = tmp.0;
-    let mut b_pieces: [Piece; 16] = tmp.1;
+    let mut board: [[Piece; 8]; 8] = init_board();
 
-    println!("{:?}", w_pieces);
+    disp_board(board);
 }
